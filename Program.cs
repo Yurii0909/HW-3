020d53fc06e8eedf4b04cfd72325be3ec77dc6db
Yurii0909 <118471966+Yurii0909@users.noreply.github.com>
2022-12-04 14:03:59 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,27 +1,28 @@
-﻿/*
-Задача 19
-Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-14212 -> нет
-12821 -> да
-23432 -> да
+/*
+Задача 21 (branch task_2)
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
 */
 
+int x1 = ReadInt("Введите координату X первой точки: ");
+int y1 = ReadInt("Введите координату Y первой точки: ");
+int z1 = ReadInt("Введите координату Z первой точки: ");
+int x2 = ReadInt("Введите координату X второй точки: ");
+int y2 = ReadInt("Введите координату Y второй точки: ");
+int z2 = ReadInt("Введите координату Z второй точки: ");
 
-Console.WriteLine("Введите число: ");
-string number = Console.ReadLine();
-int len = number.Length;
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z1 - z2;
 
-if (len == 5)
+double length = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine($"Длинна отрезка {length}");
+
+
+
+int ReadInt(string message)
 {
-    if (number[0] == number[4] && number[1] == number[3])
-    {
-        Console.WriteLine($"{number} - Да");
-    }
-    else
-    {
-        Console.WriteLine($"{number} - нет");
-    }
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
 }
-else
-
-    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
